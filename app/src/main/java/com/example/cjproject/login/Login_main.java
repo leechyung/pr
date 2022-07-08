5493005ba48a1fbb1b1d24cc648c961e6ec41b03
changhyung Lee <ckdgud0328@gmail.com>
2022-07-08 10:34:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cjproject/login/Login_main.java
@@ -0,0 +1,16 @@
+package com.example.cjproject.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cjproject.R;
+
+public class Login_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+    }
+}
---
+++ app/src/main/java/com/example/cjproject/login/Passwordpopup.java
@@ -0,0 +1,16 @@
+package com.example.cjproject.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cjproject.R;
+
+public class Passwordpopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_passwordpopup);
+    }
+}
